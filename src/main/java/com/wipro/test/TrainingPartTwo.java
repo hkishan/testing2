55ad3dbeca91ebd243bf2af1796d3dc97ee67207
hkishan <kishan.adobeday@gmail.com>
2013-04-01 12:08:15 +0530

    Update TrainingPartTwo.java
    this is my third comment

--- src/main/java/com/wipro/test/TrainingPartTwo.java
+++ src/main/java/com/wipro/test/TrainingPartTwo.java
@@ -5,5 +5,6 @@ public class TrainingPartTwo {
 	public static void main(String[] args){
 		System.out.println("I am in java");
 		System.out.println("I am in java Second Comment");
+		System.out.println("I am in java Third Comment");
 	}
 }
