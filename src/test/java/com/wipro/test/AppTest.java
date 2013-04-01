1560eff1345df24a2030d1b0f0c28e91498cae26
Hari Kishan <kishan.adobeday@gmail.com>
2013-04-01 11:06:40 +0530

    this is my first commit

---
+++ src/test/java/com/wipro/test/AppTest.java
@@ -0,0 +1,19 @@
+package com.wipro.test;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class AppTest extends TestCase{
+
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+	
+	public void testNotValid() {
+		assertFalse(false);
+	}
+
+}
